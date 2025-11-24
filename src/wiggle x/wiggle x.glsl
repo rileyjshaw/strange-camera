8223cbc598b5fa9e1a2acd6228808e72c6ce2733
Riley Shaw <rileyjshaw@gmail.com>
2025-11-24 12:12:32 -0500

    Fix aspect ratio

--- src/wiggle x/wiggle x.glsl
+++ src/wiggle x/wiggle x.glsl
@@ -9,15 +9,9 @@ uniform float x1;
 
 void main() {
 	float numColumns = 1.0 + 195.0; // TODO: Use vars. 1.0 + x1 * 195.0;
-
-	// Calculate which column this pixel is in (0 to numColumns-1)
 	float columnIndex = floor(v_uv.x * numColumns);
-
-	// Calculate delay for this column (floored to 0, starting from left)
 	int delay = int(columnIndex);
-
-	// Get the history frame based on delay and frame offset
+	vec2 texUv = fitCover(v_uv, vec2(textureSize(u_inputStream, 0).xy));
 	float z = historyZ(u_inputStream, u_inputStreamFrameOffset, delay);
-
-	outColor = texture(u_inputStream, vec3(1.0 - v_uv.x, v_uv.y, z));
+	outColor = texture(u_inputStream, vec3(1.0 - texUv.x, texUv.y, z));
 }
--- src/wiggle y/wiggle y.glsl
+++ src/wiggle y/wiggle y.glsl
@@ -11,6 +11,7 @@ void main() {
 	float numRows = 1.0 + 195.0; // TODO: Use vars. 1.0 + x1 * 195.0;
 	float rowIndex = floor(v_uv.y * numRows);
 	int delay = int(rowIndex);
+	vec2 texUv = fitCover(v_uv, vec2(textureSize(u_inputStream, 0).xy));
 	float z = historyZ(u_inputStream, u_inputStreamFrameOffset, delay);
-	outColor = texture(u_inputStream, vec3(1.0 - v_uv.x, v_uv.y, z));
+	outColor = texture(u_inputStream, vec3(1.0 - texUv.x, texUv.y, z));
 }
