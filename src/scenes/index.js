6a6c6b24cd479bd9f19e3e6996af1bd7d11d3681
Riley Shaw <rileyjshaw@gmail.com>
2026-01-03 16:48:06 -0500

    Add WIP Sunday scene

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -3,10 +3,11 @@ import bodyCamo from './body-camo';
 import channels from './channels';
 import cutup from './cutup';
 import faceCamo from './face-camo';
+import sunday from './sunday';
 import wiggleX from './wiggle x';
 import wiggleY from './wiggle y';
 
-const scenes = [bigFace, bodyCamo, channels, cutup, faceCamo, wiggleX, wiggleY].sort((a, b) =>
+const scenes = [bigFace, bodyCamo, channels, cutup, faceCamo, sunday, wiggleX, wiggleY].sort((a, b) =>
 	a.name.localeCompare(b.name)
 );
 
---
+++ src/scenes/sunday/index.js
@@ -0,0 +1,37 @@
+import ShaderPad from 'shaderpad';
+import face from 'shaderpad/plugins/face';
+import helpers from 'shaderpad/plugins/helpers';
+import save from 'shaderpad/plugins/save';
+
+import fragmentShaderSrc from './sunday.glsl';
+
+const COLOR_VALUE_INITIAL = 1;
+const GLOW_AMOUNT_INITIAL = 0;
+
+export default {
+	name: 'Sunday',
+	hash: 'sunday',
+	controls: [['Glow'], ['Color']],
+	controlValues: { x1: GLOW_AMOUNT_INITIAL, y1: COLOR_VALUE_INITIAL },
+	initialize(setShader) {
+		const shader = new ShaderPad(fragmentShaderSrc, {
+			plugins: [
+				helpers(),
+				save(),
+				face({
+					textureName: 'u_inputStream',
+					options: { maxFaces: 4 },
+				}),
+			],
+		});
+		shader.initializeUniform('u_color', 'float', COLOR_VALUE_INITIAL);
+		shader.initializeUniform('u_glow', 'float', GLOW_AMOUNT_INITIAL);
+		setShader(shader);
+	},
+	onUpdate({ x1, y1 }, shader) {
+		shader.updateUniforms({
+			u_glow: x1,
+			u_color: y1,
+		});
+	},
+};
