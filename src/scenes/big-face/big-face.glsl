e712bf2ae9e50ee6babebda3d89ea9ffe32b88a4
Riley Shaw <rileyjshaw@gmail.com>
2026-01-09 00:21:53 -0500

    Fix Big Face aliasing issue

--- src/scenes/big-face/big-face.glsl
+++ src/scenes/big-face/big-face.glsl
@@ -12,22 +12,27 @@ void main() {
 	vec2 sampleUv = uv;
 
 	for (int i = 0; i < u_nFaces; ++i) {
-		vec2 mouth = vec2(faceLandmark(i, FACE_LANDMARK_MOUTH_CENTER));
-		vec2 mouthZoomed = mouth + (uv - mouth) / u_scale;
-		vec2 mouthAt = innerMouthAt(mouthZoomed);
-		if (mouthAt.x > 0.0 && int(mouthAt.y) == i) sampleUv = mouthZoomed;
+		vec2 rightEye = vec2(faceLandmark(i, FACE_LANDMARK_R_EYE_CENTER));
+		vec2 rightEyeZoomed = rightEye + (uv - rightEye) / u_scale;
+		vec2 rightEyeResult = rightEyeAt(rightEyeZoomed);
+		if (rightEyeResult.x > 0.0 && int(rightEyeResult.y) == i) {
+			sampleUv = rightEyeZoomed;
+			break;
+		}
 
 		vec2 leftEye = vec2(faceLandmark(i, FACE_LANDMARK_L_EYE_CENTER));
-		vec2 rightEye = vec2(faceLandmark(i, FACE_LANDMARK_R_EYE_CENTER));
-		bool isLeftEyeCloser = length(uv - leftEye) <= length(uv - rightEye);
-		if (isLeftEyeCloser) {
-			vec2 leftEyeZoomed = leftEye + (uv - leftEye) / u_scale;
-			vec2 leftEyeResult = leftEyeAt(leftEyeZoomed);
-			if (leftEyeResult.x > 0.0 && int(leftEyeResult.y) == i) sampleUv = leftEyeZoomed;
-		} else {
-			vec2 rightEyeZoomed = rightEye + (uv - rightEye) / u_scale;
-			vec2 rightEyeResult = rightEyeAt(rightEyeZoomed);
-			if (rightEyeResult.x > 0.0 && int(rightEyeResult.y) == i) sampleUv = rightEyeZoomed;
+		vec2 leftEyeZoomed = leftEye + (uv - leftEye) / u_scale;
+		vec2 leftEyeResult = leftEyeAt(leftEyeZoomed);
+		if (leftEyeResult.x > 0.0 && int(leftEyeResult.y) == i) {
+			sampleUv = leftEyeZoomed;
+			break;
+		}
+
+		vec2 mouth = vec2(faceLandmark(i, FACE_LANDMARK_MOUTH_CENTER));
+		vec2 mouthZoomed = mouth + (uv - mouth) / u_scale;
+		vec2 mouthResult = outerMouthAt(mouthZoomed);
+		if (mouthResult.x > 0.0 && int(mouthResult.y) == i) {
+			sampleUv = mouthZoomed;
 		}
 	}
 
