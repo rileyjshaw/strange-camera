1e95947a383d9cedc5ff10a07b3770eb648d4c32
Riley Shaw <rileyjshaw@gmail.com>
2026-01-14 23:27:47 +0100

    Remove vite config base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import glsl from 'vite-plugin-glsl';
 
 export default {
-	base: '/strange-camera/',
 	plugins: [basicSsl(), glsl()],
 	server: {
 		https: true,
