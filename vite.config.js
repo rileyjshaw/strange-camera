0bf51c9e8a7f455dfa0107485c10bb223dd27188
Riley Shaw <rileyjshaw@gmail.com>
2026-01-15 07:31:21 +0100

    Add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import glsl from 'vite-plugin-glsl';
 
 export default {
-	plugins: [basicSsl(), glsl()],
+	plugins: [
+		basicSsl(),
+		glsl(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Strange Camera',
+				short_name: 'Strange.Cam',
+				description:
+					'Take weirder photos. Strange Camera is a free app by rileyjshaw with imaginative, playful, and unique camera filters.',
+				theme_color: '#bbff35',
+			},
+		}),
+	],
 	server: {
 		https: true,
 		host: true,
