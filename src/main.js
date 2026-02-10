021dd14116ae4dacf8fdc5aef8dda395a8f4e63c
Riley Shaw <rileyjshaw@gmail.com>
2026-02-10 07:29:56 -0500

    Shorten media access error message

--- src/main.js
+++ src/main.js
@@ -630,11 +630,11 @@ document.addEventListener('DOMContentLoaded', () => {
 				splashTitleCamera.classList.add('flicker');
 				splashMessage.textContent =
 					'There’s an error because we couldn’t get camera access. Camera permission is needed to take photos in this app. Please reload this page and grant access, or update the settings for your device or browser. Each photo / video remains 100% on your device and isn’t sent to a server.';
-				const extraMessage = document.createElement('p');
-				extraMessage.className = 'splash-message';
-				extraMessage.textContent =
-					'We took a long time writing this error message so every line has the same number of characters. Very difficult. Anyway, if you grant access to your camera and mic next time you can start actually using the fun parts of this site. Good luck. We believe in you. See you again soon!';
-				splashMessage.after(extraMessage);
+				// const extraMessage = document.createElement('p');
+				// extraMessage.className = 'splash-message';
+				// extraMessage.textContent =
+				// 	'We took a long time writing this error message so every line has the same number of characters. Very difficult. Anyway, if you grant access to your camera and mic next time you can start actually using the fun parts of this site. Good luck. We believe in you. See you again soon!';
+				// splashMessage.after(extraMessage);
 
 				splashStart.style.display = 'none';
 			}
