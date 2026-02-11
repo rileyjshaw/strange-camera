13804c4d8d969c1853e073d881647559d66d375e
Riley Shaw <rileyjshaw@gmail.com>
2026-02-11 18:19:37 -0500

    Apply blur to segment mask

--- src/scenes/blurryface/blurryface.glsl
+++ src/scenes/blurryface/blurryface.glsl
@@ -7,16 +7,34 @@ uniform sampler2D u_inputStream;
 uniform sampler2D u_blurred;
 uniform float u_mode;
 
+float getMask(vec2 uv, int frame) {
+	vec2 seg = segmentAt(uv, frame);
+	return seg.x * (1.0 - seg.y);
+}
+
+float blurMask(vec2 uv, int frame) {
+	vec2 o = 10.0 / u_resolution;
+	return (
+		getMask(uv, frame) * 4.0
+		+ getMask(uv + vec2( o.x, 0.0), frame) * 2.0
+		+ getMask(uv + vec2(-o.x, 0.0), frame) * 2.0
+		+ getMask(uv + vec2(0.0,  o.y), frame) * 2.0
+		+ getMask(uv + vec2(0.0, -o.y), frame) * 2.0
+		+ getMask(uv + vec2( o.x,  o.y), frame)
+		+ getMask(uv + vec2(-o.x,  o.y), frame)
+		+ getMask(uv + vec2( o.x, -o.y), frame)
+		+ getMask(uv + vec2(-o.x, -o.y), frame)
+	) / 16.0;
+}
+
 void main() {
 	vec2 texSize = vec2(textureSize(u_inputStream, 0));
 	vec2 uv = fitCover(v_uv, texSize);
 	vec3 camColor = texture(u_inputStream, uv).rgb;
 	vec3 blurredColor = texture(u_blurred, uv).rgb;
 
-	vec2 currentSegment = segmentAt(uv, 0);
-	vec2 previousSegment = segmentAt(uv, 1);
-	float maskCurr = currentSegment.x * (1.0 - currentSegment.y);
-	float maskPrev = previousSegment.x * (1.0 - previousSegment.y);
+	float maskCurr = blurMask(uv, 0);
+	float maskPrev = blurMask(uv, 1);
 	float maskSmoothed = mix(maskPrev, maskCurr, 0.35);
 	float mask = smoothstep(0.15, 0.5, maskSmoothed);
 
